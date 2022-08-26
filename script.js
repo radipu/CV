c0e2ac54f8c25d36ae7598ed474dc2110c23a15f
radipu <rad.edu.du@gmail.com>
2022-08-26 20:16:37 +0600

    updated the intro typed

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Software Engineer", "Author", "Designer", "Freelancer"],
+        strings: ["Software Engineer", ".NET Developer", "Full Stack Web App Developer", "Author", "Designer", "Freelancer", "Techie"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Software Engineer", "Author", "Designer", "Freelancer"],
+        strings: ["Software Engineer", ".NET Developer", "Full Stack Web App Developer", "Author", "Designer", "Freelancer", "Techie"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true

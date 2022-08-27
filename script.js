6cfa19be2ec4d6233d3f77d0db20e9aa4771630b
radipu <rad.edu.du@gmail.com>
2022-08-27 21:28:02 +0600

    update typo designation

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Software Engineer", ".NET Developer", "Full Stack Web App Developer", "Author", "Designer", "Freelancer", "Techie"],
+        strings: ["Software Engineer", ".NET Developer", "Full Stack Web and Desktop App Developer", "Author", "Designer", "Freelancer", "Techie"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Software Engineer", ".NET Developer", "Full Stack Web App Developer", "Author", "Designer", "Freelancer", "Techie"],
+        strings: ["Software Engineer", ".NET Developer", "Full Stack Web and Desktop App Developer", "Author", "Designer", "Freelancer", "Techie"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true

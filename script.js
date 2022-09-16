16da7ec6ac6759b180a412f522a88b930edf1851
radipu <rad.edu.du@gmail.com>
2022-09-16 12:14:36 +0600

    updated the portfolio, ommit pagination for a while

--- script.js
+++ script.js
@@ -82,73 +82,111 @@ $(document).ready(function(){
 
 //pagination
 
-function getPageList(totalPages, page, maxLength){
-  function range(start, end){
-    return Array.from(Array(end - start + 1), (_, i) => i + start);
-  }
+// function getPageList(totalPages, page, maxLength){
+//   function range(start, end){
+//     return Array.from(Array(end - start + 1), (_, i) => i + start);
+//   }
 
-  var sideWidth = maxLength < 9 ? 1 : 2;
-  var leftWidth = (maxLength - sideWidth * 2 - 3) >> 1;
-  var rightWidth = (maxLength - sideWidth * 2 - 3) >> 1;
+//   var sideWidth = maxLength < 9 ? 1 : 2;
+//   var leftWidth = (maxLength - sideWidth * 2 - 3) >> 1;
+//   var rightWidth = (maxLength - sideWidth * 2 - 3) >> 1;
 
-  if(totalPages <= maxLength){
-    return range(1, totalPages);
-  }
+//   if(totalPages <= maxLength){
+//     return range(1, totalPages);
+//   }
 
-  if(page <= maxLength - sideWidth - 1 - rightWidth){
-    return range(1, maxLength - sideWidth - 1).concat(0, range(totalPages - sideWidth + 1, totalPages));
-  }
+//   if(page <= maxLength - sideWidth - 1 - rightWidth){
+//     return range(1, maxLength - sideWidth - 1).concat(0, range(totalPages - sideWidth + 1, totalPages));
+//   }
 
-  if(page >= totalPages - sideWidth - 1 - rightWidth){
-    return range(1, sideWidth).concat(0, range(totalPages - sideWidth - 1 - rightWidth - leftWidth, totalPages));
-  }
+//   if(page >= totalPages - sideWidth - 1 - rightWidth){
+//     return range(1, sideWidth).concat(0, range(totalPages - sideWidth - 1 - rightWidth - leftWidth, totalPages));
+//   }
 
-  return range(1, sideWidth).concat(0, range(page - leftWidth, page + rightWidth), 0, range(totalPages - sideWidth + 1, totalPages));
-}
+//   return range(1, sideWidth).concat(0, range(page - leftWidth, page + rightWidth), 0, range(totalPages - sideWidth + 1, totalPages));
+// }
 
-$(function(){
-  var numberOfItems = $(".portfolios .portfolio-item").length;
-  var limitPerPage = 6;
-  var totalPages =  Math.ceil(numberOfItems / limitPerPage);
-  var paginationSize = 7;
-  var currentPage;
+// $(function(){
+//   var numberOfItems = $(".portfolios .portfolio-item").length;
+//   var limitPerPage = 6;
+//   var totalPages =  Math.ceil(numberOfItems / limitPerPage);
+//   var paginationSize = 7;
+//   var currentPage;
 
-  function showPage(whichPage){
-    if(whichPage < 1 || whichPage > totalPages) return false;
+//   function showPage(whichPage){
+//     if(numberOfItems <= 6){
+//       $(".pagination").hide
+//     }
+//     if(whichPage < 1 || whichPage > totalPages) return false;
 
-    currentPage = whichPage;
+//     currentPage = whichPage;
 
-    $(".portfolios .portfolio-item").hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show();
+//     $(".portfolios .portfolio-item").hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show();
 
-    $(".pagination li").slice(1, -1).remove();
+//     $(".pagination li").slice(1, -1).remove();
 
-    getPageList(totalPages, currentPage, paginationSize).forEach(item => {
-      $("<li>").addClass("page-item").addClass(item ? "current-page" : "dots")
-      .toggleClass("active", item === currentPage).append($("<a>").addClass("page-link")
-      .attr({href: "javascript:void(0)"}).text(item || "...")).insertBefore(".next-page");
-    });
+//     getPageList(totalPages, currentPage, paginationSize).forEach(item => {
+//       $("<li>").addClass("page-item").addClass(item ? "current-page" : "dots")
+//       .toggleClass("active", item === currentPage).append($("<a>").addClass("page-link")
+//       .attr({href: "javascript:void(0)"}).text(item || "...")).insertBefore(".next-page");
+//     });
 
-    $(".previous-page").toggleClass("disable", currentPage === 1);
-    $(".next-page").toggleClass("disable", currentPage === totalPages);
-    return true;
-  }
+//     $(".previous-page").toggleClass("disable", currentPage === 1);
+//     $(".next-page").toggleClass("disable", currentPage === totalPages);
+//     return true;
+//   }
 
-  $(".pagination").append(
-    $("<li>").addClass("page-item").addClass("previous-page").append($("<a>").addClass("page-link").text("Prev")),
-    $("<li>").addClass("page-item").addClass("next-page").append($("<a>").addClass("page-link").text("Next"))
-  );
+//   $(".pagination").append(
+//     $("<li>").addClass("page-item").addClass("previous-page").append($("<a>").addClass("page-link").text("Prev")),
+//     $("<li>").addClass("page-item").addClass("next-page").append($("<a>").addClass("page-link").text("Next"))
+//   );
 
-  $(".portfolios").show();
-  showPage(1);
+//   $(".portfolios").show();
+//   showPage(1);
 
-  $(document).on("click", ".pagination li.current-page:not(.active)", function(){
-    return showPage(+$(this).text());
-  });
+//   $(document).on("click", ".pagination li.current-page:not(.active)", function(){
+//     return showPage(+$(this).text());
+//   });
 
-  $(".previous-page").on("click", function(){
-    return showPage(currentPage - 1);
-  });
-  $(".next-page").on("click", function(){
-    return showPage(currentPage + 1);
-  });
-});
+//   $(".previous-page").on("click", function(){
+//     return showPage(currentPage - 1);
+//   });
+//   $(".next-page").on("click", function(){
+//     return showPage(currentPage + 1);
+//   });
+// });
+
+//poftfolio classified buttons
+const FilterContainer = document.querySelector(".portfolio-filter"),
+            filterBtns = FilterContainer.children;
+            totalFilterBtn = filterBtns.length;
+            PortfolioItems = document.querySelectorAll(".portfolio-item"),
+            console.log(PortfolioItems)
+            totalportfolioItem = PortfolioItems.length;
+            for(let i=0; i < totalFilterBtn; i++)
+            {
+                filterBtns[i].addEventListener("click", function()
+                {
+                    FilterContainer.querySelector(".active").classList.remove("active");
+                    this.classList.add("active");
+                    const filterValue = this.getAttribute("data-filter")
+                    for( let k=0; k<totalportfolioItem; k++)
+                    {
+                        if(filterValue === PortfolioItems[k].getAttribute("data-category"))
+                        {
+                            PortfolioItems[k].classList.remove("hide");
+                            PortfolioItems[k].classList.add("show");
+                        }
+                        else
+                        {
+                            PortfolioItems[k].classList.remove("show");
+                            PortfolioItems[k].classList.add("hide");
+                        }
+                        if(filterValue === "all")
+                        {
+                            PortfolioItems[k].classList.remove("hide");
+                            PortfolioItems[k].classList.add("show");
+                        }
+                    }
+                })
+            }

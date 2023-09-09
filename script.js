12d64c429b8b30b22d00efca546b4a9fc8317ee1
Rohul Amin Mohammad Dipu <rad.edu.du@gmail.com>
2023-09-10 00:12:05 +0600

    updated google analytic code

--- script.js
+++ script.js
@@ -360,4 +360,4 @@ window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-R5SLKL374R');
+  gtag('config', 'G-C718TJ3PZ7');
